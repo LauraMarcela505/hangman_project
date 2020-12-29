99b8f129f22202b532627083217de6e7cd114768
laura <Laura.Marcela505@gmail.com>
2020-12-29 03:03:06 -0500

    added some new things + word list

--- hangman.py
+++ hangman.py
@@ -1,62 +1,65 @@
-# Python Hangman Game
+#Python Hangman Game
+# 12/28/2020
+# it's about to be the 29th bc it's really late
 
-
-# how many guesses the player has
-
-# def guesses():
-#     player_guess = int(input("how many guesses do you want to have (5-15)?\n"))
-#     if player_guess > 16 or player_guess <= 4:
-#     if player_guess > 16 or player_guess <= 4:
-#         print("that's not between 1-20")
-#         return guesses()
-#     else:
-#         return player_guess
-#
-#
-# print("the number of guesses you have to play this game is ", guesses())
-
-
-# choose a random word
-
+#libraries
 import random
 import time
 
-random_words = ("watar","airpad","bathraom","spaan")
-word = random.choice(random_words)
+# http://www.mit.edu/~ecprice/wordlist.10000
+
+def random_word():
+    with open("wordlist.10000.txt","r") as f:
+        text_file = f.read()
+        words = list(text_file.split())
+        word = random.choice(words)
+    return word
+
+
+def guesses():
+    player_guess = int(input("how many guesses do you want to have (5-15)?\n"))
+    if player_guess > 16 or player_guess <= 4:
+        print("that's not between 5-15")
+        return guesses()
+    else:
+        return player_guess
 
 
 def player_guess():
-    word = random.choice(random_words)
+    word = random_word()
     print(word)
-    word_length = len(word)
-    disguised_word = word_length * '_'
+    disguised_word = len(word) * '_'
+
     word = list(word) # this is the new part
     disguised_word = list(disguised_word)
-    print(disguised_word)
-    guess = 5
+
+    guess = 0
+    num_of_guesses = guesses()
     already_guessed = []
-    while guess < 10:
+
+    while guess < num_of_guesses:
         player_guessing = input("guess your first letter")
         player_guessing = player_guessing.lower()
 
         if player_guessing in word and player_guessing not in already_guessed:
             already_guessed.append(player_guessing)
-            # print("you already entered that letter, try a new one")
             index_pos = [i for i, x in enumerate(word) if x == player_guessing]
-            print(index_pos)
-            print(player_guessing)
+
             for i in index_pos:
                 disguised_word[i] = player_guessing
             print(''.join(disguised_word))
+
+            print("You have", num_of_guesses, "tries left.")
+
+
         elif player_guessing in already_guessed:
             print("you already entered that letter, try a new one")
 
         else:
-
             guess = guess + 1
-            print("oh no wrong")
-
-
+            print("oh no, letter not in word!")
+            print("You have", (num_of_guesses-guess), "tries left")
+            print(''.join(disguised_word))
 
 print(player_guess())
 
